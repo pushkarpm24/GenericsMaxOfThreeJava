5eb47b8bba1cfb9c3e98a2e0749440ec70500f1b
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-05 14:02:01 +0530

    ADDED:code to print the max using generic method

--- src/main/java/com/bridgelabz/maximumOfThree/MaximumOfThree.java
+++ src/main/java/com/bridgelabz/maximumOfThree/MaximumOfThree.java
@@ -3,16 +3,22 @@ package com.bridgelabz.maximumOfThree;
 import java.util.Arrays;
 
 public class MaximumOfThree<E extends Comparable<E>> {
-
+    // Array arguments of type E
     E[] arguments;
+
+    // Constructor for initialization of array elements
     public MaximumOfThree(E[] array) {
         this.arguments=array;
 
     }
+    // method to find maximum in given array
 public static  <E extends Comparable<E>> E findMaximum(E... arguments) {
     Arrays.sort(arguments);
     return  arguments[arguments.length-1];
 }
-
+    // method to print maximum from given array
+    private static <E extends Comparable<E>> void printMax(E maximum) {
+        System.out.println("Maximum is "+maximum);
+    }
 
 }
